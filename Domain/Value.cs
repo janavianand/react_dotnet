15d6b3c4517bd7fa041d0be798dac7dff85b65ce
Janavi Anand <janavi@Admins-MBP.home>
2020-04-01 14:34:54 -0400

    add files

---
+++ Domain/Value.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        /* Auto implemented properties 
+        
+        Entity framework - convention based - Id is automatically used as a primary key
+        */
+
+        public int Id {get;set;}
+        public string Name {get;set;}
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        // constructor
+        // need to mention the base class options for migrations - causes probs in migrations if we dont
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values {get;set;}
+
+        //override a method available in dbcontext
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value {Id=1,Name="Value 101"},
+                    new Value {Id=2,Name="Value 102"},
+                    new Value {Id=3,Name="Value 103"}
+                );
+        }
+    }
+}
